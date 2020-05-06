0e7229d050d6a4fb06fea1d442afc20cbd273db9
Jacob Fletcher <jacobsfletch@gmail.com>
2020-05-06 14:53:42 -0400

    returns black from hexToRGBA as safe fallback

--- src/hexToRGBA/index.js
+++ src/hexToRGBA/index.js
@@ -1,20 +1,22 @@
 const hexToRGBA = (hex, opacity) => {
-  const sanitizedHex = hex.replace('#', '');
-
   let r = 0;
   let g = 0;
   let b = 0;
 
-  if (sanitizedHex.length === 3) {
-    r = parseInt(`${sanitizedHex.substring(0, 1)}${sanitizedHex.substring(0, 1)}`, 16);
-    g = parseInt(`${sanitizedHex.substring(1, 2)}${sanitizedHex.substring(1, 2)}`, 16);
-    b = parseInt(`${sanitizedHex.substring(2, 3)}${sanitizedHex.substring(2, 3)}`, 16);
-  }
+  if (hex && typeof hex === 'string') {
+    const sanitizedHex = hex.replace('#', '');
 
-  if (sanitizedHex.length === 6) {
-    r = parseInt(sanitizedHex.substring(0, 2), 16);
-    g = parseInt(sanitizedHex.substring(2, 4), 16);
-    b = parseInt(sanitizedHex.substring(4, 6), 16);
+    if (sanitizedHex.length === 3) {
+      r = parseInt(`${sanitizedHex.substring(0, 1)}${sanitizedHex.substring(0, 1)}`, 16);
+      g = parseInt(`${sanitizedHex.substring(1, 2)}${sanitizedHex.substring(1, 2)}`, 16);
+      b = parseInt(`${sanitizedHex.substring(2, 3)}${sanitizedHex.substring(2, 3)}`, 16);
+    }
+
+    if (sanitizedHex.length === 6) {
+      r = parseInt(sanitizedHex.substring(0, 2), 16);
+      g = parseInt(sanitizedHex.substring(2, 4), 16);
+      b = parseInt(sanitizedHex.substring(4, 6), 16);
+    }
   }
 
   return `rgba(${r}, ${g}, ${b}, ${opacity || 1})`;
